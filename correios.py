8aa07f85371f2eb319b3c74987889f615d0b4d23
Luiz <junqueira.luizcarlo@gmail.com>
2018-05-23 21:57:24 -0300

    test mock - app.config['CORS_HEADERS']  Content-Type

--- correios.py
+++ correios.py
@@ -4,22 +4,50 @@ correios.py
 ----------
     Api para usar dados dos Correios
 """
-from flask import Flask, request
+from flask import Flask, request, Response
+from flask.ext.cors import CORS, cross_origin
 import requests
 import json
 import os
 from bson.json_util import dumps
 from pymongo import MongoClient
-client = MongoClient(os.environ.get('DOCKER_HOST'), 27017)
-db = client.test_database
-enderecos = db.enderecos
-pessoas = db.pessoas
+# client = MongoClient(os.environ.get('DOCKER_HOST'), 27017)
+# db = client.test_database
+# enderecos = db.enderecos
+# pessoas = db.pessoas
 
 __version__ = '0.1.0'
 __author__ = {
     'Luiz Junqueira': 'lcjneto@gmail.com',
 }
 app = Flask(__name__)
+cors = CORS(app, resources={r"/foo": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route('/broker/auth', methods=['GET', 'POST', 'OPTIONS'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def auth():
+	data = {"token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsY2puZXRvQGdtYWlsLmNvbSIsImV4cCI6MTUyMDczMTA5NX0.4ggbTz0fJxB6NcxLDIY9wOige-19aTsSQDdn31SmncK6ggY1wRsFPdax4DmKKjXHVz6iylLfeYYloaP4EUsm1w",
+			"user": {"email": "lcjneto@gmail.com",
+			"last_login": "2018-03-04 01:04:04.589",
+			"name": "roo",
+			"username": "lcjneto@gmail.com"}
+	}
+	resp = Response(json.dumps(data))
+	# resp.headers['Access-Control-Allow-Credentials'] = 'true'
+	return resp
+
+
+@app.route('/broker/profile', methods=['GET', 'POST', 'OPTIONS'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def profile():
+	data = {"hasSecurityPhrase": "false",
+			"hasTwoFactor": "false",
+			"localCurrencyLimit": '0',
+			"securityPhrase": ''}
+	resp = Response(json.dumps(data))
+	return resp
 
 
 @app.route('/correios/<cep>', methods=['GET'])
